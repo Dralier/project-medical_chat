8f40819d08d85f8a576bf52fc11841b33003d4ad
Dralier <axel.racca@gmail.com>
2021-10-20 15:44:03 -0300

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css';
+import React, { useState } from 'react';
+import { StreamChat } from 'stream-chat';
+import { Chat } from 'stream-chat-react';
+import Cookies from 'universal-cookie';
+
+import {ChannelListContainer, ChannelContainer }from './components';
+
+const apiKey = 'p849y623vr2g';
+const client = StreamChat.getInstance(apiKey)
+
+const App = () => {
+  return (
+    <div className="app__wrapper">
+     <Chat client={client} theme="team light">
+       <ChannelListContainer />
+       <ChannelContainer />
+     </Chat>
+    </div>
+  );
+}
+
+export default App;
