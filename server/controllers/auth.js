9e723f28379efb8f7ae723b5ae7cfb17f675551b
Dralier <axel.racca@gmail.com>
2021-10-27 18:01:18 -0300

    banda

---
+++ server/controllers/auth.js
@@ -0,0 +1,27 @@
+const { connect } = require('getstream');
+const bcrypt = require('bcrypt');
+const StreamChat = require('stream-chat').StreamChat;
+const crypto = require('crypto');
+
+require('dotenv').config();
+
+const login = (req, res) => {
+    try {
+        
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ message: error });
+    }
+};
+const signup = (req, res) => {
+    try {
+        const { fullName, username, password, phoneNumber } = req.body;
+
+        const userId = crypto
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ message: error });
+    }
+};
+
+module.exports = { signup, login };
