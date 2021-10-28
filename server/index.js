778aa4bf9b3329d145151eb82bc501be623f8c08
Dralier <axel.racca@gmail.com>
2021-10-28 15:21:55 -0300

    more backend

--- server/index.js
+++ server/index.js
@@ -7,8 +7,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 require('dotenv').config();
-
-app.use(cors());
+app.use(
+    cors({
+        methods: "*",
+        origin: "*",
+        allowedHeaders: "*",
+    })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
